736d13e51b4cffa9385dd22756e7a05073104bbe
Daniel Tuttle <danieltuttle@Daniels-MacBook-Pro-2.local>
2022-10-08 21:47:14 -0700

    progress

--- main/main.go
+++ main/main.go
@@ -7,40 +7,51 @@ import (
 
 func main() {
 	args := os.Args
-
-	if len(args) != 6 {
-		printHelp()
-	} else if len(args) == 6 {
+	if len(args) > 1 {
 		prog := args[1]
-		firstFlag := args[2]
-		firstValue := args[3]
-		secondFlag := args[4]
-		secondValue := args[5]
-		if prog == "line-search" && firstFlag == "-s" && len(firstValue) > 0 && secondFlag == "-p" && len(secondValue) > 0 {
-			res, e := modules.GetAllFilesForLocation(secondValue)
-			if e == nil {
-				for _, e := range res {
-					res, e := modules.GetLinesThatMatchFromFile(e, firstValue)
+		if prog == "pretty-json" {
+			if len(args) > 3 {
+				firstFlag := args[2]
+				firstValue := args[3]
+				if firstFlag == "-s" && len(firstValue) > 0 {
+					s := modules.GetPrettyJson(firstValue)
+					modules.WriteFileWithContents(s, firstValue)
+				}
+			}
+		}
+
+		if prog == "line-search" {
+			if len(args) > 5 {
+				firstFlag := args[2]
+				firstValue := args[3]
+				secondFlag := args[4]
+				secondValue := args[5]
+				if firstFlag == "-s" && len(firstValue) > 0 && secondFlag == "-c" && len(secondValue) > 0 {
+					res, e := modules.GetAllFilesForLocation(firstValue)
 					if e == nil {
 						for _, e := range res {
-							print(e)
+							res, e := modules.GetLinesThatMatchFromFile(e, secondValue)
+							if e == nil {
+								for _, e := range res {
+									print(e)
+								}
+							}
 						}
 					}
 				}
 			}
-		} else if prog == "pretty-json" && firstFlag == "-s" && len(firstValue) > 0 && secondFlag == "-d" && len(secondValue) > 0 {
-			s := modules.GetPrettyJson(firstValue)
-			modules.WriteFileWithContents(s, secondValue)
-		} else {
-			printHelp()
 		}
+	} else {
+		printHelp()
 	}
 }
 
 func printHelp() {
 	println("")
 	println("Dan's random cli")
+	println("")
 	println("Options:")
+	println("")
 	println("line-search -s <search-string> -p <absolute/path/to/folder/location>")
 	println("pretty-json -s <absolute-soure-file-path> -d <absolute-destination-file-path>")
 	println("")
